5d7fdf7c3a02cba80b3f20a9f5a47457536c4dcb
Dylan Bray <dfbray@utexas.edu>
2020-11-22 16:03:13 -0600

    Skeleton maven project

---
+++ Add/src/main/java/org/swevolution/add/Add.java
@@ -0,0 +1,7 @@
+package org.swevolution.add;
+
+public class Add {
+    public static int doAddition(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Calculator/src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+import org.swevolution.add.Add;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.print(Add.doAddition(a, b));
+    }
+}
