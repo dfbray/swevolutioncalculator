3fc3005eb57ddad1b39c8160317d7b1c5c5817dd
unknown <alex.issa32@utexas.edu>
2020-11-22 21:13:02 -0600

    Added rest of code

--- Calculator/src/main/java/Calculator.java
+++ Calculator/src/main/java/Calculator.java
@@ -1,9 +1,26 @@
 import org.swevolution.add.Add;
+import org.swevolution.subtract.Subtract;
+import org.swevolution.multiply.Multiply;
+import org.swevolution.divide.Divide;
 
 public class Calculator {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        System.out.print(Add.doAddition(a, b));
+        System.out.println("Correct answer is 7, our add answer is " + Add.doAddition(4, 3));
+        System.out.println("Correct answer is 1, our subtract answer is " + Subtract.doSubtraction(4, 3));
+
+        System.out.println("Correct answer is 12, our multiply answer is " + Multiply.doMultiplication(4, 3));
+        System.out.println("Correct answer is 0, our multiply answer is " + Multiply.doMultiplication(0, 0));
+        System.out.println("Correct answer is -2, our multiply answer is " + Multiply.doMultiplication(2, -1));
+        System.out.println("Correct answer is -2, our multiply answer is " + Multiply.doMultiplication(-2, 1));
+        System.out.println("Correct answer is 2, our multiply answer is " + Multiply.doMultiplication(-2, -1));
+        try {
+            System.out.println("Correct answer is 1r1, our divide answer is " + Divide.doDivision(4, 3));
+            System.out.println("Correct answer is -20, our divide answer is " + Divide.doDivision(60, -3));
+            System.out.println("Correct answer is 0r-3, our divide answer is " + Divide.doDivision(-3, 6));
+            System.out.println("Correct answer is 1, our divide answer is " + Divide.doDivision(-3, -3));
+            System.out.println("Correct answer is Exception, our divide answer is " + Divide.doDivision(4, 0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
